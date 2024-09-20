2fae8c119639732cc990143e2c1fd5788de20ca1
foyinjohn <fasasifoyin@gmail.com>
2024-09-20 17:17:55 +0100

    update - search

--- controllers/Search.js
+++ controllers/Search.js
@@ -1,77 +1,60 @@
+import createHttpError from "http-errors";
 import Blog from "../models/Blogs.js";
 import Tags from "../models/Tags.js";
-import expressAsyncHandler from "express-async-handler";
 
-export const search = expressAsyncHandler(async (req, res) => {
-  const LIMIT = 10;
-  const page = Number(req.query.page) || 1;
+const buildSearchQuery = (search, searchTags, title, body) => {
+  const searchQuery = {
+    tags: { $in: searchTags },
+  };
 
-  const search = { $regex: req.query.search, $options: "i" } || "";
-  let title = req.query.title || "true";
-  let body = req.query.body || "false";
-  let category = req.query.category || "false";
-  let categories = req.query.categories || "All";
+  if (title === "true") {
+    searchQuery.$or = [{ title: { $regex: search, $options: "i" } }];
+  }
 
-  const getCategories = await Tags.find();
+  if (body === "true") {
+    searchQuery.$or = [{ body: { $regex: search, $options: "i" } }];
+  }
 
-  if (categories === "All") {
-    categories = getCategories.map((each) => each.tag);
-  } else {
-    const check = req.query.categories
-      .split(",")
-      .every((element) => getCategories.some(({ tag }) => tag === element));
-    if (!check) {
-      return res.status(400).json({ message: "There is an error" });
+  if (body === "true" && title === "true") {
+    searchQuery.$or = [
+      { title: { $regex: search, $options: "i" } },
+      { body: { $regex: search, $options: "i" } },
+    ];
+  }
+  return searchQuery;
+};
+
+export const search = async (req, res, next) => {
+  try {
+    const { title, body, tags, search, page } = req.query;
+    console.log(title,body)
+
+    if (!search || search === "undefined") {
+      throw createHttpError(400, "No search");
     }
-    categories = req.query.categories.split(",");
+
+    if (title !== "true" && body !== "true") {
+      throw createHttpError(400, "Select either title or body or both");
+    }
+
+    const LIMIT = 10;
+    const PAGE = Number(page) || 1;
+    const SKIP = (page - 1) * LIMIT;
+
+    let searchTags = [];
+
+    if (!tags || tags === "undefined") {
+      const getAllTags = await Tags.find();
+      searchTags = getAllTags.map((each) => each.tag);
+    } else {
+      searchTags = tags.split(",");
+    }
+
+    const searchQuery = buildSearchQuery(search, searchTags, title, body);
+    const searchResults = await Blog.find(searchQuery);
+
+    res.status(200).json(searchResults);
+  } catch (error) {
+    next(error);
   }
-
-  let searchResult = [];
-
-  if (title === "true" && body === "false" && category === "false") {
-    searchResult = await Blog.find({
-      $and: [{ title: search }, { tags: { $in: categories } }],
-    });
-  } else if (title === "false" && body === "true" && category === "false") {
-    searchResult = await Blog.find({
-      $and: [{ body: search }, { tags: { $in: categories } }],
-    });
-  } else if (title === "false" && body === "false" && category === "true") {
-    searchResult = await Blog.find({
-      tags: { $in: [req.query.search] },
-    }).collation({ locale: "en", strength: 2 });
-  } else if (title === "true" && body === "true" && category === "false") {
-    searchResult = await Blog.find({
-      $or: [
-        { $and: [{ title: search }, { tags: { $in: categories } }] },
-        { $and: [{ body: search }, { tags: { $in: categories } }] },
-      ],
-    });
-  } else if (title === "true" && body === "false" && category === "true") {
-    searchResult = await Blog.find({
-      $or: [
-        { $and: [{ title: search }, { tags: { $in: categories } }] },
-        { tags: { $in: [req.query.search] } },
-      ],
-    }).collation({ locale: "en", strength: 2 });
-  } else if (title === "false" && body === "true" && category === "true") {
-    searchResult = await Blog.find({
-      $or: [
-        { $and: [{ body: search }, { tags: { $in: categories } }] },
-        { tags: { $in: [req.query.search] } },
-      ],
-    }).collation({ locale: "en", strength: 2 });
-  } else if (title === "true" && body === "true" && category === "true") {
-    searchResult = await Blog.find({
-      $or: [
-        { $and: [{ title: search }, { tags: { $in: categories } }] },
-        { $and: [{ body: search }, { tags: { $in: categories } }] },
-        { tags: { $in: [req.query.search] } },
-      ],
-    }).collation({ locale: "en", strength: 2 });
-  } else {
-    searchResult = [];
-  }
-
-  return res.status(200).json(searchResult);
-});
+};
